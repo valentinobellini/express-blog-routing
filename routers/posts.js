c54c778110c7aa0b61abc5de9c0f0ecfe0ed24c6
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-05 15:14:09 +0100

    create data/posts

--- routers/posts.js
+++ routers/posts.js
@@ -47,31 +47,40 @@ router.get('/', function (req, res) {
     res.json(posts);
 
 
-    res.send('Lista dei posts');
 });
+
+
 // show
 router.get('/:id', function (req, res) {
+
+    
     res.send('Dettagli del post ' + req.params.id);
 });
+
+
 // store
 router.post('/', function (req, res) {
     res.send('Creazione nuovo post');
 });
+
 
 // update
 router.put('/:id', function (req, res) {
     res.send('Modifica integrale del post ' + req.params.id);
 });
 
+
 // modify
 router.patch('/:id', function (req, res) {
     res.send('Modifica parziale del post ' + req.params.id);
 });
 
+
 // destroy
 router.delete('/:id', function (req, res) {
     res.send('Eliminazione del post ' + req.params.id);
 });
+
 
 // esportiamo il modulo del router
 module.exports = router;
